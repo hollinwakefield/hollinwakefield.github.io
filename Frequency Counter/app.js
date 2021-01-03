89dbec9013bfbd5ac8bc20a7f4e633b80fdc6b8e
hollinwakefield <hollinwakefield@gmail.com>
2021-01-03 02:55:42 -0600

    updated to count words

--- Frequency Counter/app.js
+++ Frequency Counter/app.js
@@ -1,31 +1,53 @@
-//let str = "with the urban tribe, or our parents, until our brains just mature on their own and somehow suddenly know the sure answers to our lives. But that’s not how the brain works. And that’s not how life works. Besides, even if our brains could wait, love and work can’t. The twenties are, indeed, the time to get busy. It’s forward thinking for an uncertain age.";
+// let str = "with the urban tribe, or our parents, until our brains just mature on their own and somehow suddenly know the sure answers to our lives. But that’s not how the brain works. And that’s not how life works. Besides, even if our brains could wait, love and work can’t. The twenties are, indeed, the time to get busy. It’s forward thinking for an uncertain age.";
 
-let tastiness = document.querySelector('h1')
-tastiness.innerHTML = '<strong>Disgustin</strong>'
 
-// str = str.replace(/[^\w\s\'\’]|_/g, "")
-//     .replace(/\s+/g, " ");
-// let array = str.split(' ');
-// array = array.map(function(x){ return x.toUpperCase(); })
-// console.log(array);
+function getWordArray() {
+    // Get input from form
+    let inputString = document.querySelectorAll('input')[0].value
+    
+    // Remove undesired characters from the string
+    inputString = inputString.replace(/[^\w\s\'\’]|_/g, "")
+        .replace(/\s+/g, " ");
 
-// function getWordCount() {
-//     let map = {};
-//   for (let i = 0; i < array.length; i++) {
-//     let item = array[i];
-//     map[item] = (map[item] + 1) || 1;
-//   }
-  
-//   let freqRank = [];
-//   for (let word in map) {
-//       freqRank.push([word, map[word]]);
-//   }
+    // Convert string into array of strings
+    let wordListArray = inputString.split(' ');
 
-//   freqRank.sort(function(a, b) {
-//       return b[1] - a[1];
-//   });
+    // Convert each string in array to uppercase
+    wordListArray = wordListArray.map(function(x){ return x.toUpperCase(); })
+    // console.log(`The word list array: ${wordListArray}`);
+    return getWordCount(wordListArray); 
+}
 
-// getWordCount(array); // { apple: 3}, {orange: 2}
+function getWordCount(wordListArray) {
+    // Add each string to a counting object
+    let map = {};
+    for (let i = 0; i < wordListArray.length; i++) {
+        let item = wordListArray[i];
+        map[item] = (map[item] + 1) || 1;
+    }
+    
+    // Convert object into array of objects for each key:value pair
+    let freqRank = [];
+    for (let word in map) {
+        freqRank.push([word, map[word]]);
+    }
+
+    // Sort the array from most frequent to least frequent
+    freqRank.sort(function(a, b) {
+        return b[1] - a[1];
+    });
+    console.log(`The frequency rank object: ${freqRank}`)
+    return freqRank;
+}
+
+window.onload=function(){
+    const count = document.querySelector('#countInput')
+    count.addEventListener('submit', function(e) {
+        e.preventDefault();
+        getWordArray();
+    });
+}
+// getWordCount(wordListArray); // { apple: 3}, {orange: 2}
 
 // let str = document.getElementById("count")
 
